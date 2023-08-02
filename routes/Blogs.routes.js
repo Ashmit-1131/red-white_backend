900f6b9964b2d487a893ae5a9e94c7766a2c6803
Ashmit Yadav <beingashmit@gmail.com>
2023-08-02 17:35:29 +0530

    ahsh

--- routes/Blogs.routes.js
+++ routes/Blogs.routes.js
@@ -7,18 +7,11 @@ require("dotenv").config();
 
 blogRouter.get("/", async (req, res) => {
   try {
-    const token = req.headers.authorization;
-    const page = req.query.page || 0;
-    const decoded = jwt.verify(token, process.env.SecretKey);
-    const { userId: user } = decoded;
-
-    const count = await BlogsModel.countDocuments({ user });
-    const data = await BlogsModel.find({ user }).skip(page * 5).limit(5);
+    const data = await BlogsModel.find();
 
     res.send({
-      message: "All cart data",
+      message: "All blog data",
       status: 1,
-      count: count,
       data: data,
       error: false,
     });
@@ -30,6 +23,7 @@ blogRouter.get("/", async (req, res) => {
     });
   }
 });
+
 
 blogRouter.get("/:pid", async (req, res) => {
   try {
