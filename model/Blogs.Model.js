2748883f142a4168a9b588ca03c7cc73ee5dc45f
Ashmit Yadav <beingashmit@gmail.com>
2023-08-02 08:34:00 +0530

    first commit

---
+++ model/Blogs.Model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+
+const blogsSchema = mongoose.Schema({
+    image:{type:String},
+    title:{type:String},
+    description:{type:String},
+    user:{type:String,required:true},
+  pid:{type:String},
+ 
+
+
+    
+
+},{
+    versionKey:false
+})
+
+
+const BlogsModel = mongoose.model("/blog",blogsSchema)
+
+module.exports={
+    BlogsModel
+}
---
+++ routes/Blogs.routes.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { cartNorderValidator } = require("../middlewares/cart&orderValidator");
+const { BlogsModel } = require("../model/Blogs.Model");
+const blogRouter = express.Router();
+require("dotenv").config();
+
+blogRouter.get("/", async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const page = req.query.page || 0;
+    const decoded = jwt.verify(token, process.env.SecretKey);
+    const { userId: user } = decoded;
+
+    const count = await BlogsModel.countDocuments({ user });
+    const data = await BlogsModel.find({ user }).skip(page * 5).limit(5);
+
+    res.send({
+      message: "All cart data",
+      status: 1,
+      count: count,
+      data: data,
+      error: false,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
+
+blogRouter.get("/:pid", async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const { pid } = req.params;
+    const decoded = jwt.verify(token, process.env.SecretKey);
+    const { userId: user } = decoded;
+
+    const data = await BlogsModel.find({ user, pid });
+
+    if (data.length > 0) {
+      res.send({
+        message: "Item already in cart",
+        status: 1,
+        error: false,
+      });
+    } else {
+      res.send({
+        message: "Item not present in cart",
+        status: 0,
+        error: true,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
+
+blogRouter.patch("/:id", async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.SecretKey);
+    const { userId: user } = decoded;
+
+    await BlogsModel.updateOne({ _id, user }, req.body);
+
+    res.send({
+      message: "Item updated",
+      status: 1,
+      error: false,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
+
+blogRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, process.env.SecretKey);
+    const { userId: user } = decoded;
+
+    await BlogsModel.deleteOne({ _id, user });
+
+    res.send({
+      message: "Item deleted",
+      status: 1,
+      error: false,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Something went wrong: " + error.message,
+      status: 0,
+      error: true,
+    });
+  }
+});
+
+blogRouter.use(cartNorderValidator);
+
+blogRouter.post("/add", async (req, res) => {
+  try {
+    await BlogsModel.insertMany(req.body);
+    res.send({
+        message: "Item added in blogs",
+        status: 1,
+        error: false,
+      });
+    } catch (error) {
+        res.status(500).send({
+        message: "Something went wrong: " + error.message,
+        status: 0,
+        error: true,
+        });
+        }
+        });
+        
+        module.exports = {
+        blogRouter,
+        }
