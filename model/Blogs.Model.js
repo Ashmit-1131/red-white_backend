49d659327999e0e602c8b629ab468d7cf322e8a3
Ashmit Yadav <beingashmit@gmail.com>
2023-08-19 18:04:00 +0530

    userdata

--- model/Blogs.Model.js
+++ model/Blogs.Model.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String },
-    checklist: { type: String },
-    comments: { type: String },
-    project: { type: String },
-    assign: [{ type: String }],
-    dueDate: { type: Date },
-    labels: { type: String },
-    status: { type: String, default: 'created' }, // Default status is 'created'
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-  },
-  {
-    versionKey: false
-  }
+    {
+
+        name: { type: String, required: true },
+        description: { type: String },
+        checklist: { type: String },
+        comments: { type: String },
+        project: { type: String },
+        assign: [{ type: String }],
+        dueDate: { type: Date },
+        labels: { type: String },
+        user:[{type:String,required:true}]
+    },
+    {
+        versionKey: false
+    }
 );
 
 const TaskModel = mongoose.model("task", TaskSchema);
 
 module.exports = {
-  TaskModel
+    TaskModel
 };
